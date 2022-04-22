b834380641797920ee073c24fe63c24d3ea8aee5
Enno Gotthold <egotthold@suse.de>
2022-04-22 17:06:07 +0200

    Increase version to 3.3.3

--- tests/settings/migrations/migrations_test.py
+++ tests/settings/migrations/migrations_test.py
@@ -5,6 +5,7 @@ Tests for the Cobbler settings migrations
 # SPDX-FileCopyrightText: 2021 Dominik Gedon <dgedon@suse.de>
 # SPDX-FileCopyrightText: 2021 Enno Gotthold <egotthold@suse.de>
 # SPDX-FileCopyrightText: Copyright SUSE LLC
+
 import pytest
 import yaml
 
@@ -21,6 +22,7 @@ from cobbler.settings.migrations import (
     V3_0_0,
     V3_3_1,
     V3_3_2,
+    V3_3_3,
     V3_4_0,
 )
 
@@ -240,3 +242,55 @@ def test_migrate_v3_4_0():
 
     # Assert
     assert V3_4_0.validate(new_settings)
+
+
+def test_migrate_v3_3_2():
+    # Arrange
+    with open("/code/tests/test_data/V3_3_1/settings.yaml") as old_settings:
+        old_settings_dict = yaml.safe_load(old_settings.read())
+
+    # Act
+    new_settings = V3_3_2.migrate(old_settings_dict)
+
+    # Assert
+    assert V3_3_2.validate(new_settings)
+
+
+def test_normalize_v3_3_2():
+    # Arrange
+    with open("/code/tests/test_data/V3_3_2/settings.yaml") as old_settings:
+        old_settings_dict = yaml.safe_load(old_settings.read())
+
+    # Act
+    new_settings = V3_3_2.normalize(old_settings_dict)
+
+    # Assert
+    assert len(V3_3_2.normalize(new_settings)) == 129
+
+
+def test_migrate_v3_3_3():
+    # Arrange
+    with open("/code/tests/test_data/V3_3_2/settings.yaml") as old_settings:
+        old_settings_dict = yaml.safe_load(old_settings.read())
+
+    # Act
+    new_settings = V3_3_3.migrate(old_settings_dict)
+
+    # Assert
+    assert V3_3_3.validate(new_settings)
+    # Migration of default_virt_file_size to float is working
+    assert isinstance(new_settings.get("default_virt_file_size", None), float)
+
+
+def test_normalize_v3_3_3():
+    # Arrange
+    with open("/code/config/cobbler/settings.yaml") as old_settings:
+        old_settings_dict = yaml.safe_load(old_settings.read())
+
+    # Act
+    new_settings = V3_3_3.normalize(old_settings_dict)
+
+    # Assert
+    assert len(V3_3_3.normalize(new_settings)) == 129
+    # Migration of default_virt_file_size to float is working
+    assert isinstance(new_settings.get("default_virt_file_size", None), float)
